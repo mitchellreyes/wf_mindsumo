1dae99d95ceba38a25567c0c7626e14820075eec
mitchellreyes <mitchell.reyes32@gmail.com>
2017-09-25 14:44:22 -0700

    added more comments, but it is done

--- data_analysis.py
+++ data_analysis.py
@@ -3,8 +3,18 @@ from sklearn import tree
 
 #file where the decision tree information will print to
 output_file = 'custom_report.txt'
+
+#will hold the original data set. Used in print_data_report() for printing training data
 training_data_for_print = read_data()
-#breaks up data to fit the DecisionTreeClassifier().fit
+
+'''
+@fn: get_decision_tree_data()
+@params:
+    cust_dict is the the customer list from read_data, passed to by create_decision_tree()
+@result:
+    creates sets of data in training_data and create_decision_tree() will use the indexed lists as
+    training data
+'''
 def get_decision_tree_data(cust_dict = {}):
     training_data = []
     if len(cust_dict) > 0:
@@ -20,12 +30,48 @@ def get_decision_tree_data(cust_dict = {}):
         training_data.append(s_balances)
         training_data.append(decision)
     return training_data
+
 '''
-1) Makes a prediction to open/close a checking account based off of customers' checking balances
-    a) This data is taken from the original customers who closed an account over the 6 months,
-        will be able to refine when sample size is larger
-2) If prediction is 'close an account', it will make a prediction again based off of customers' saving balances
-3) Prints all this data to a file, along with their preferred contact method.
+@fn: print_data_report()
+@params: 
+    1) file_name is passed from create_decision_tree() -> output_file
+    2) c_list is the customer list, passed from create_decision_tree()
+    3) print_training_data lets you decide if you want to print out what the decision tree was trained on
+        3a) prints the data from train_data
+    4) train_data is the original data set holding the training data
+@result: prints data from decision tree to file_name
+'''
+def print_data_report(c_list = {}, file_name = '', print_training_data = True, train_data = {}):
+    if (len(c_list) > 0) and (file_name != ''):
+        f = open(file_name, 'w')
+
+        if print_training_data:
+            f.write('TRAINING DATA USED:\n')
+            for masked_id in train_data:
+                if train_data[masked_id].get_checking_status() != '':
+                    f.write('\t[masked_id: %s, age: %s]\n' % (masked_id, c_list[masked_id].age))
+                    f.write('\t\tchecking_balances: ' + str(c_list[masked_id].get_check_balances()) + '\n')
+                    f.write('\t\tsavings_balances: ' + str(c_list[masked_id].get_sav_balances()) + '\n')
+                    f.write('\t[Decision: %s]\n\n' % c_list[masked_id].get_checking_status())
+            f.write('\n\n')
+
+        f.write('PREDICTIONS MADE BY DECISION TREE:\n')
+        for masked_id in c_list:
+            f.write('\t[masked_id: %s, age: %s]\n' % (masked_id, c_list[masked_id].age))
+            f.write('\t\tcheckings_balances: ' + str(c_list[masked_id].get_check_balances()) + '\n')
+            f.write('\t\tsavings_balances: ' + str(c_list[masked_id].get_sav_balances()) + '\n')
+            f.write('\t[Decision: %s]\n' % (c_list[masked_id].get_checking_status()))
+            f.write('\t[Preferred contact method: %s]\n\n' % c_list[masked_id].pref_contact_medium[:-4])
+        f.close()
+
+'''
+@fn: create_decision_tree()
+@result:
+    1) Makes a prediction to open/close a checking account based off of customers' checking balances
+        a) This data is taken from the original customers who closed an account over the 6 months,
+            will be able to refine when sample size is larger
+    2) If prediction is 'close an account', it will make a prediction again based off of customers' saving balances
+    3) Prints all this data to a file, along with their preferred contact method.
 '''
 def create_decision_tree():
     #gets the customer list w preprocessed data -> {1 : c, 2: c,...}
@@ -57,36 +103,7 @@ def create_decision_tree():
 
 
     print_data_report(c_list = customer_list, file_name = output_file, train_data = training_data_for_print)
-'''
-prints data from decision tree to file_name
-@params: file_name is passed from create_decision_tree() -> output_file
-        c_list is the customer list, passed from create_decision_tree()
-'''
-def print_data_report(c_list = {}, file_name = '', print_training_data = True, train_data = {}):
-    if (len(c_list) > 0) and (file_name != ''):
-        f = open(file_name, 'w')
-
-        if print_training_data:
-            f.write('TRAINING DATA USED:\n')
-            for masked_id in train_data:
-                if train_data[masked_id].get_checking_status() != '':
-                    f.write('\t[masked_id: %s, age: %s]\n' % (masked_id, c_list[masked_id].age))
-                    f.write('\t\tchecking_balances: ' + str(c_list[masked_id].get_check_balances()) + '\n')
-                    f.write('\t\tsavings_balances: ' + str(c_list[masked_id].get_sav_balances()) + '\n')
-                    f.write('\t[Decision: %s]\n\n' % c_list[masked_id].get_checking_status())
-            f.write('\n\n')
-
-        f.write('PREDICTIONS MADE BY DECISION TREE:\n')
-        for masked_id in c_list:
-            f.write('\t[masked_id: %s, age: %s]\n' % (masked_id, c_list[masked_id].age))
-            f.write('\t\tcheckings_balances: ' + str(c_list[masked_id].get_check_balances()) + '\n')
-            f.write('\t\tsavings_balances: ' + str(c_list[masked_id].get_sav_balances()) + '\n')
-            f.write('\t[Decision: %s]\n' % (c_list[masked_id].get_checking_status()))
-            f.write('\t[Preferred contact method: %s]\n\n' % c_list[masked_id].pref_contact_medium[:-4])
-        f.close()
 
 
-
-
-
+#running the decision tree
 create_decision_tree()
